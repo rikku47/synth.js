c1a46e9f8e85ed1d101d0f58a8664415cdb776cb
Kai Friebe <kaifriebe@kai.fritz.box>
2020-12-03 16:37:31 +0100

    Moved to js/synth/

---
+++ js/synth/synth.js
@@ -0,0 +1,127 @@
+function parseNoteValues(){
+    return JSON.parse('notevalues.js');
+}
+
+function square() {
+
+    var points = [];
+
+    for (let index = 0; index <= 360; index++) {
+
+        if (index == 0) {
+
+            var point = {
+                x: index,
+                y: 200 + 100
+            };
+
+            points.push(point);
+
+        } else if (index == 180) {
+
+            var point = {
+                x: index,
+                y: 200 + 100
+            };
+
+            points.push(point);
+
+            var point = {
+                x: index,
+                y: 200 - 100
+            };
+
+            points.push(point);
+
+        } else if (index == 360) {
+
+            var point = {
+                x: index,
+                y: 200 - 100
+            };
+
+            points.push(point);
+
+            var point = {
+                x: index,
+                y: 200
+            };
+
+            points.push(point);
+
+        }
+    }
+
+    return points;
+}
+
+function saw() {
+
+    var points = [];
+
+    for (let index = 0; index <= 360; index++) {
+
+        if (index == 0) {
+
+            var point = {
+                x: index,
+                y: 200 + 100
+            };
+
+            points.push(point);
+
+        } else if (index == 180) {
+
+            var point = {
+                x: index,
+                y: 200 - 100
+            };
+
+            points.push(point);
+
+            var point = {
+                x: index,
+                y: 200 + 100
+            };
+
+            points.push(point);
+
+        } else if (index == 360) {
+
+            var point = {
+                x: index,
+                y: 200 - 100
+            };
+            points.push(point);
+
+            var point = {
+                x: index,
+                y: 200
+            };
+
+            points.push(point);
+
+        }
+    }
+
+    return points;
+}
+
+function triangle() {
+
+    var points = [];
+
+    for (let index = 0; index <= 360; index++) {
+
+        var point = {
+            x: index,
+            y: 200
+        };
+
+        points.push(point);
+
+    }
+
+    return points;
+
+}
