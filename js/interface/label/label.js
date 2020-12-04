5bc8d57abe88eb2632a4786814a2900d48b11eee
Kai Friebe <kaifriebe@kai.fritz.box>
2020-12-04 17:14:08 +0100

    Added properies.

--- js/interface/label/label.js
+++ js/interface/label/label.js
@@ -1,6 +1,12 @@
-function createLabel(id) {
+function createLabel(id, forr, text) {
 
     let label = document.createElement('label');
+
+    label.id = id;
+
+    label.setAttribute('for', forr);
+
+    label.textContent = text;
 
     return label;
 
